eeb532ceb84b8e84a06de9c38ec0d24a848c42a9
LBB <>
2019-10-15 22:45:21 +0200

    Añadidas clases Distancia.java y DistanciaDoblePrecision.java

---
+++ ProgramaCero/src/programacero/DistanciaDoblePrecision.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacero;
+
+/**
+ *
+ * @author luis
+ */
+public class DistanciaDoblePrecision extends Distancia {
+
+    double distancia; //Este atributo sobreescribe al de la superclase Distancia
+    public DistanciaDoblePrecision(){distancia=0;}
+    public DistanciaDoblePrecision(double d){this.distancia=d;}
+    
+}
